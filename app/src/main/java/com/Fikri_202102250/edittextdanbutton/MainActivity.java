6d2fbfee6678a0202f80bee418a4be6f01c6df1d
heeren17 <miyukichan003@gmail.com>
2023-03-15 10:31:34 +0700

    Initial commit

---
+++ app/src/main/java/com/Fikri_202102250/edittextdanbutton/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Fikri_202102250.edittextdanbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
